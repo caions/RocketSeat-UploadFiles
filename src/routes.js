466be96270501dff2d041f7adbf51f31a2e82686
unknown <caionsantos2000@gmail.com>
2020-01-08 10:52:29 -0300

    feature delete and list post

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,13 @@ const multer = require('multer')
 const multerConfig = require('./config/multer')
 const Upload = require('../db/uploads')
 
+// exibir posts
+routes.get('/posts',async (req,res)=>{
+    const posts = await Upload.findAll()
+    return res.json(posts)
+})
+
+//criar posts 
 routes.post('/posts', multer(multerConfig).single('file'), async(req, res) => {
     // const {originalname: name, size , filename: key} = req.file //desestruturação 
     const upload = await Upload.create({
@@ -13,6 +20,18 @@ routes.post('/posts', multer(multerConfig).single('file'), async(req, res) => {
     })
 
     return res.json(upload)
+})
+
+//deletar posts
+routes.get('/posts/:id',(req,res)=>{
+    Upload.destroy({
+        where:{
+            id: req.params.id
+        }
+    }).then(()=>{
+        res.send('Post deletado')
+    })
+    
 })
 
 module.exports = routes
