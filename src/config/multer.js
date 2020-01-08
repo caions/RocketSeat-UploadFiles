665c717198bd8f658ec7c0fd5ef4c31c68be3481
unknown <caionsantos2000@gmail.com>
2020-01-08 00:45:44 -0300

    upload to sever ok v3 Final

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,13 +4,6 @@ const crypto = require('crypto')
 const aws = require('aws-sdk')
 const multerS3 = require('multer-s3')
 
-const s3 = new aws.S3({
-	accessKeyId: 'AKIAQOVG7X7SE4QND5WQ',
-	secretAccessKey: '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',
-    Bucket: 'uploadcaions',
-    defaultRegion: 'us-east-1',
-});
-
 const storageTypes = {
     local: multer.diskStorage({
         destination: (req, file, cb) => {
@@ -27,25 +20,23 @@ const storageTypes = {
         }
     }),
     s3: multerS3({
-        s3: s3,
-		bucket: 'uploadcaions',
-		acl: 'public-read',
+        s3: new aws.S3(),
+        bucket: 'uploadcaions',
         contentType: multerS3.AUTO_CONTENT_TYPE,
-        key: (req, file, cb) => {
-          crypto.randomBytes(16, (err, hash) => {
-            if (err) cb(err);
-    
-            const fileName = `${hash.toString("hex")}-${file.originalname}`;
-    
-            cb(null, fileName);
-          });
-        }
-      })
-    };
+        acl: 'public-read',
+        key: (req,file,cb) =>{
+            crypto.randomBytes(5, (erro, hash) => { //quantidade de bytes de caracteres antes do nome
+                if (erro) cb(erro)
+                const filename = `${hash.toString('hex')}-${file.originalname}`;
+                cb(null, filename)
+            })
+        },
+    }),
+}
 
 module.exports = {
     dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: storageTypes["s3"],
+    storage: storageTypes['s3'],
     limits: {
         fileSize: 2 * 1024 * 1024
     },
